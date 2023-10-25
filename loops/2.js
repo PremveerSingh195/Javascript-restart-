9a6919e2a1948c4c2c92dc1366e40c0ab459eebc
PremveerSingh195 <prem.veer2836@gmail.com>
2023-10-25 16:35:31 +0530

    loops

---
+++ loops/2.js
@@ -0,0 +1,9 @@
+for (let i = 2; i < 20; i++) {
+    const even = i;
+
+    if (i%2 === 0) {
+        console.log(even);
+    } 
+    
+}
+
---
+++ loops/3.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 10 ; i++) {
+    const x = i*5
+    console.log(x);
+}
